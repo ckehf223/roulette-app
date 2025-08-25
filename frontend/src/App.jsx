4a2d0c9dc3253ac887241ae23b3229cb40b611d4
차재경 <ckehf223@gmail.com>
2025-08-25 17:14:13 +0900

    add ga4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,23 @@ import Resulthis from './components/Resulthis';
 import RegisterMember from './components/RegisterMember';
 import ProtectedRoute from './common/ProtectedRoute';
 import Login from './components/Login';
+import usePageTracking from './common/usePageTracking';
+import ReactGA from "react-ga4";
 
 Modal.setAppElement('#root'); // 접근성 설정용 (필수)
 
 function App() {
+  useEffect(() => {
+    // 발급받은 GA4 측정 ID 입력
+    const gaId = import.meta.env.VITE_GA_ID;
+    if (gaId) {
+      ReactGA.initialize(gaId);
+      ReactGA.send("pageview"); // 첫 페이지 로딩 시 기록
+    }
 
+  }, []);
+
+  usePageTracking();
   return (
     <>
       <AuthProvider>
