1a940ddb1d6d605543394372b628d1edb030ac01
차재경 <ckehf223@gmail.com>
2025-09-11 17:28:00 +0900

    deploy test3

--- backend/src/main/java/com/spring/board/auth/jwt/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/spring/board/auth/jwt/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +17,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtUtil jwtUtil;
@@ -30,6 +32,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+
+        String path = request.getRequestURI();
+        log.info("===========================================================================");
+        log.info("path = " + path);
+        if (path.startsWith("/api/auth/")
+                || path.startsWith("/api/res/")
+                || path.startsWith("/api/member/")
+                || path.equals("/api/result/his/save")
+                || path.equals("/")) {
+            filterChain.doFilter(request, response); // 그냥 통과
+            return;
+        }
+        log.info("===========================================================================");
 
         String header = request.getHeader("Authorization");
 
