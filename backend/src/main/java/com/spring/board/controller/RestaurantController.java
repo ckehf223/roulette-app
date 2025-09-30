aeea1f1ee3769f6222c1b4954e0272e093712d87
ckehf223 <ckehf223@gmail.com>
2025-09-30 21:16:20 +0900

    spelling error clear

--- backend/src/main/java/com/spring/board/controller/RestaurantController.java
+++ backend/src/main/java/com/spring/board/controller/RestaurantController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/ap")
+@RequestMapping("/api/sharelist")
 public class RestaurantController {
 
     private final RestaurantService restaurantService;
