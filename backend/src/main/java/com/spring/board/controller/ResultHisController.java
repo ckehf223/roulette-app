cdf2c7e70d9d12f021cb10a34a8332171fd94f4d
차재경 <ckehf223@gmail.com>
2025-10-21 11:25:25 +0900

    cacheing error clear

--- backend/src/main/java/com/spring/board/controller/ResultHisController.java
+++ backend/src/main/java/com/spring/board/controller/ResultHisController.java
@@ -24,11 +24,10 @@ public class ResultHisController {
     public ResponseEntity<List<CamelCaseMap>> findResultHisList(ResultHisDto resultHisDto){
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails user = (CustomUserDetails) auth.getPrincipal();
-        if(user != null && "ADMIN".equals(user.getRole())){
-            resultHisDto.setUserId(null);
-        }else if(user != null && "USER".equals(user.getRole())){
+
+        if(user != null && user.getRole() != null){
             resultHisDto.setUserId(user.getUserId());
-        }else {
+        }else{
             return ResponseEntity.ok(null);
         }
 
